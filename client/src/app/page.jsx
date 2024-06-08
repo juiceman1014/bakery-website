7b72fa3c5bb37ba66e9110a5e3346de17103ec0b
Danh Pham <danhpham612@gmail.com>
2024-06-08 11:40:13 -0700

    test nav component

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,10 +1,12 @@
-import Link from 'next/link'
+import Link from "next/link";
+import Nav from "@/components/navbar/nav";
 
 export default function Home() {
   return (
     <main>
-    <h1>Hello how are you</h1>
-    <Link href = "/profile">Profile</Link>
+      <h1>Hello how are you</h1>
+      <Link href="/profile">Profile</Link>
+      <Nav />
     </main>
   );
 }
