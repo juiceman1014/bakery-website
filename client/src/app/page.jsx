433a9af23e8ac290e5c28871fc7d7efe6c917216
juiceman1014 <justintran1014@gmail.com>
2024-06-09 22:44:26 -0700

    removed test code

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -2,11 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex-col">
-      <div className=" bg-sky-800">01</div>
-      <div className=" bg-sky-400">02</div>
-      <div className=" bg-sky-800">03</div>
-      <div className=" bg-sky-400">04</div>
-    </div>
+    <h1 className="flex flex-row">
+    </h1>
   );
 }
