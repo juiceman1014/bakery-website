f1b6644715332ebb38dbc409db084e3bc30281a0
juiceman1014 <justintran1014@gmail.com>
2024-06-09 23:40:23 -0700

    completed navbar structure

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,9 +2,24 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className = "flex flex-col">
-      <div>
-        <p>LOGO</p>
+    
+    <div className = "h-screen">
+      <div className = "flex flex-col justify-between h-full">
+
+        <div className>LOGO</div>
+
+        <div>Menu/Order</div>
+
+        <div>
+          <p>About</p>
+          <p>Contact</p>
+          <p>Login</p>
+        </div>
+
+        <div>CART</div>
+
+        <div>Translate</div>
+
       </div>
     </div>
   );
