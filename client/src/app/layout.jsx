a0d617bd396e41400df42825ba43630d413170ee
juiceman1014 <justintran1014@gmail.com>
2024-06-09 23:40:54 -0700

    layout takes 100% of page

--- client/src/app/layout.jsx
+++ client/src/app/layout.jsx
@@ -12,11 +12,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html className = "min-h-full" lang="en">
       <body className={inter.className}>
         <Navbar />
         {children}
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   );
