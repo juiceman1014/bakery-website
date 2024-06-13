ae07a9114ba5aa6876f8aeb7e6b393f969b9fe49
juiceman1014 <justintran1014@gmail.com>
2024-06-12 19:28:53 -0700

    added more css styling to the navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,31 +3,31 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="h-screen">
-      <div className="flex flex-col justify-between h-full w-24 bg-orange-100 items-center">
+      <div className="flex flex-col justify-between h-full w-24 bg-orange-100 items-center py-4">
 
-        <div>
+        <div className="hover:bg-orange-200 p-2 rounded-full">
           <button>
-            <img className = "h-20" src="/mooncake-sample-logo.jpeg" alt="Mooncake Logo"/>
+            <img className="h-16 w-16 rounded-full" src="/mooncake-sample-logo.jpeg" alt="Mooncake Logo"/>
           </button>
         </div>
 
         <div>
-          <button>Menu/Order</button>
+          <button className="hover:bg-orange-200 p-2 rounded-md text-sm font-medium">Menu/Order</button>
         </div>
 
-        <div className = "flex flex-col items-start">
-          <button>About</button>
-          <button>Contact</button>
-          <button>Login</button>
+        <div className="flex flex-col space-y-2">
+          <button className="hover:bg-orange-200 p-2 rounded-md text-sm font-medium">About</button>
+          <button className="hover:bg-orange-200 p-2 rounded-md text-sm font-medium">Contact</button>
+          <button className="hover:bg-orange-200 p-2 rounded-md text-sm font-medium">Login</button>
         </div>
 
-        <div>
+        <div className="hover:bg-orange-200 p-2 rounded-full">
           <button>
-            <img className = "h-20" src="/cart-sample-logo.jpeg" alt="Cart Logo"/>
+            <img className="h-16 w-16 rounded-full" src="/cart-sample-logo.jpeg" alt="Cart Logo"/>
           </button>
         </div>
 
-        <div>
+        <div className="hover:bg-orange-200 p-2 rounded-md text-sm font-medium">
           <button>Translate</button>
         </div>
 
