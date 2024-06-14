7d01876e12c5d1d171a41eca87ad69d23fe15476
Danh Pham <danhpham612@gmail.com>
2024-06-14 15:33:59 -0700

    modified video

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,20 +1,19 @@
 import Link from "next/link";
 
 export default function Home() {
-
- 
-
   return (
     <>
-      <div>
-        <video
-          className="h-screen"
-          src="/cake.mp4"
-          type="video/mp4"
-          autoPlay
-          muted
-          loop
-        ></video>
+      <div className="flex flex-col">
+        <div>
+          <video
+            className="h-full w-full object-cover"
+            src="/cake.mp4"
+            type="video/mp4"
+            autoPlay
+            muted
+            loop
+          ></video>
+        </div>
       </div>
     </>
   );
