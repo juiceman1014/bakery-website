48884b3990dcbb9d77518c65b1db1a451e7f81e2
juiceman1014 <justintran1014@gmail.com>
2024-06-14 15:12:08 -0700

    added video to intro

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,16 +1,19 @@
 import Link from "next/link";
 
 export default function Home() {
+
+ 
+
   return (
     <>
       <div>
         <video
-          className="h-full"
+          className="h-screen"
           src="/cake.mp4"
           type="video/mp4"
-          width="600"
-          height="400"
-          controls
+          autoPlay
+          muted
+          loop
         ></video>
       </div>
     </>
