b4e0b9c040803929cc4b9c58b7d8a862acf170d6
Danh Pham <danhpham612@gmail.com>
2024-06-16 18:38:12 -0700

    able to scroll content

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,27 +3,42 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="h-screen">
-      <div className="flex flex-col justify-between h-full w-24 bg-orange-100 items-center py-4">
-
+      <div className="flex flex-col justify-between h-full w-24 bg-orange-100 items-center py-4 fixed">
         <div className="hover:bg-orange-200 p-2 rounded-full">
           <button>
-            <img className="h-16 w-16 rounded-full" src="/mooncake-sample-logo.jpeg" alt="Mooncake Logo"/>
+            <img
+              className="h-16 w-16 rounded-full"
+              src="/mooncake-sample-logo.jpeg"
+              alt="Mooncake Logo"
+            />
           </button>
         </div>
 
         <div>
-          <button className="hover:bg-orange-200 p-2 rounded-md text-sm font-medium">Menu/Order</button>
+          <button className="hover:bg-orange-200 p-2 rounded-md text-sm font-medium">
+            Menu/Order
+          </button>
         </div>
 
         <div className="flex flex-col space-y-2">
-          <button className="hover:bg-orange-200 p-2 rounded-md text-sm font-medium">About</button>
-          <button className="hover:bg-orange-200 p-2 rounded-md text-sm font-medium">Contact</button>
-          <button className="hover:bg-orange-200 p-2 rounded-md text-sm font-medium">Login</button>
+          <button className="hover:bg-orange-200 p-2 rounded-md text-sm font-medium">
+            About
+          </button>
+          <button className="hover:bg-orange-200 p-2 rounded-md text-sm font-medium">
+            Contact
+          </button>
+          <button className="hover:bg-orange-200 p-2 rounded-md text-sm font-medium">
+            Login
+          </button>
         </div>
 
         <div className="hover:bg-orange-200 p-2 rounded-full">
           <button>
-            <img className="h-16 w-16 rounded-full" src="/cart-sample-logo.jpeg" alt="Cart Logo"/>
+            <img
+              className="h-16 w-16 rounded-full"
+              src="/cart-sample-logo.jpeg"
+              alt="Cart Logo"
+            />
           </button>
         </div>
 
@@ -34,7 +49,6 @@ const Navbar = () => {
         <div className="hover:bg-orange-200 p-2 rounded-md text-sm font-medium">
           <button>Anh Danh © 2024</button>
         </div>
-
       </div>
     </div>
   );
