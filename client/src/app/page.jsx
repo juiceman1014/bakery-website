eab08a0a054b2963ed8e043cedcd1876cc15b240
Danh Pham <danhpham612@gmail.com>
2024-06-16 18:38:12 -0700

    able to scroll content

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -3,16 +3,57 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-        <div>
-          <video
-            className="h-full w-full object-cover"
-            src="/cake.mp4"
-            type="video/mp4"
-            autoPlay
-            muted
-            loop
-          ></video>
-        </div>
+      <div>
+        <video
+          className="h-full w-full object-cover"
+          src="/cake.mp4"
+          type="video/mp4"
+          autoPlay
+          muted
+          loop
+        ></video>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+        <h1 className="text-center">test</h1>
+      </div>
     </>
   );
 }
