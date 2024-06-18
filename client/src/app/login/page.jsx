13273dd0c528d61a55a8d7684a5a34079f809337
juiceman1014 <justintran1014@gmail.com>
2024-06-17 19:29:05 -0700

    added initial code

---
+++ client/src/app/login/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const login = () => {
+    return(
+        <div>
+           <p>HELLO</p>  
+           <p>HELLO</p>     
+           <p>HELLO</p>     
+           <p>HELLO</p>        
+        </div>
+    )
+}
+
+export default login
