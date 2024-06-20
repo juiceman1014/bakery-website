faaf0731fa90f8bd4af6c88c1fbf6b695927b950
juiceman1014 <justintran1014@gmail.com>
2024-06-20 15:29:11 -0700

    changed color

--- client/src/app/checkout-methods/page.jsx
+++ client/src/app/checkout-methods/page.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Checkout = () => {
     return(
         <div className = "flex flex-row justify-center items-center h-screen">
-        <container className = "flex flex-col justify-center items-center bg-[green] h-3/6 w-6/12 justify-evenly">
-            <button className = "h-1/6 w-6/12 bg-[blue]">Pickup</button>
-            <button className = "h-1/6 w-6/12 bg-[blue]">Delivery</button>
+        <container className = "flex flex-col justify-center items-center bg-orange-100 h-3/6 w-6/12 justify-evenly">
+            <button className = "h-1/6 w-6/12 bg-slate-100">Pickup</button>
+            <button className = "h-1/6 w-6/12 bg-slate-100">Delivery</button>
         </container>
         </div>
     );
