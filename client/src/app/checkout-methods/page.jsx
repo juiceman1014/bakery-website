a87179f039c282c464f7457ab1193d89ae6e211b
juiceman1014 <justintran1014@gmail.com>
2024-06-20 15:19:09 -0700

    initial page layout

--- client/src/app/checkout-methods/page.jsx
+++ client/src/app/checkout-methods/page.jsx
@@ -2,7 +2,12 @@ import React from "react";
 
 const Checkout = () => {
     return(
-        <p>Hello</p>
+        <div className = "flex flex-row justify-center items-center h-screen">
+        <container className = "flex flex-col items-center bg-[green] h-6/12 w-6/12">
+            <div>Hello</div>
+            <div>Hello</div>
+        </container>
+        </div>
     );
 };
 
