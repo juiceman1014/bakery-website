c43b343d0a58225480538c43ffb3fbc02e67800b
Danh Pham <danhpham612@gmail.com>
2024-06-20 16:02:30 -0700

    modified button and link for checkout methods

--- client/src/app/checkout-methods/page.jsx
+++ client/src/app/checkout-methods/page.jsx
@@ -1,25 +1,26 @@
 import React from "react";
-import Link from "next/link"
+import Link from "next/link";
 
 const Checkout = () => {
-    return(
-        <div className = "flex flex-row justify-center items-center h-screen">
-        <container className = "flex flex-col justify-center items-center bg-orange-100 h-3/6 w-6/12 justify-evenly">
-
-        
-        <Link href = "/checkout-methods/pickup">
-            <button className = "bg-slate-100 w-[100px] h-[100px]">
-                Pickup
-            </button>
+  return (
+    <div className="flex flex-row justify-center items-center h-screen">
+      <container className="flex flex-col justify-center items-center bg-orange-100 h-3/6 w-6/12 justify-evenly">
+        <Link
+          href="/checkout-methods/pickup"
+          className="h-1/6 w-6/12 bg-slate-100 flex justify-center"
+        >
+          <button>Pick up</button>
         </Link>
-        
 
-        <button className = "h-1/6 w-6/12 bg-slate-100">
-            <Link href = "/checkout-methods/delivery">Delivery</Link>
-        </button>
-        </container>
-        </div>
-    );
+        <Link
+          href="/checkout-methods/delivery"
+          className="h-1/6 w-6/12 bg-slate-100 flex justify-center"
+        >
+          <button>Delivery</button>
+        </Link>
+      </container>
+    </div>
+  );
 };
 
-export default Checkout
+export default Checkout;
