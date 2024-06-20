dc051b349e3cf8b2b104e2e3d37ba00cf43532e6
juiceman1014 <justintran1014@gmail.com>
2024-06-20 15:26:32 -0700

    updated styling

--- client/src/app/checkout-methods/page.jsx
+++ client/src/app/checkout-methods/page.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Checkout = () => {
     return(
         <div className = "flex flex-row justify-center items-center h-screen">
-        <container className = "flex flex-col items-center bg-[green] h-6/12 w-6/12">
-            <div>Hello</div>
-            <div>Hello</div>
+        <container className = "flex flex-col justify-center items-center bg-[green] h-3/6 w-6/12 justify-evenly">
+            <button className = "h-1/6 w-6/12 bg-[blue]">Pickup</button>
+            <button className = "h-1/6 w-6/12 bg-[blue]">Delivery</button>
         </container>
         </div>
     );
