e7d30e7b02795aeb160be9e826b2c480dccd6e82
juiceman1014 <justintran1014@gmail.com>
2024-06-24 16:48:12 -0700

    initial pickup page code

--- client/src/app/checkout-methods/pickup/page.jsx
+++ client/src/app/checkout-methods/pickup/page.jsx
@@ -1,11 +1,30 @@
 import React from "react";
+import Link from "next/link";
 
 const Pickup = () => {
-    return(
-        <div>
-            Pickup
-        </div>
-    );
+  return (
+    <>
+      <div className="flex flex-row justify-center items-center h-screen">
+        <container className="flex flex-col justify-center items-start bg-orange-100 h-[500px] w-[500px] justify-evenly my-auto">
+          <div className="flex flex-col ml-[3rem]">
+            <p>Pickup Name: </p>
+            <input className = "w-[300px]"></input>
+          </div>
+
+          <div className="flex flex-col ml-[3rem]">
+            <p>Email Address: </p>
+            <input className = "w-[300px]" ></input>
+          </div>
+
+          <div className="flex flex-col ml-[3rem]">
+            <p>Phone Number: </p>
+            <input className = "w-[300px]"></input>
+          </div>
+
+        </container>
+      </div>
+    </>
+  );
 };
 
-export default Pickup
+export default Pickup;
