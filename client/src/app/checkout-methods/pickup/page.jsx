e9f6b3eaf2e8e335d13ca550e0b3cab1cce3053d
juiceman1014 <justintran1014@gmail.com>
2024-06-26 15:10:46 -0700

    added submit button

--- client/src/app/checkout-methods/pickup/page.jsx
+++ client/src/app/checkout-methods/pickup/page.jsx
@@ -8,25 +8,25 @@ const Pickup = () => {
         <container className="flex flex-col justify-center items-start bg-orange-100 h-[500px] w-[500px] justify-evenly">
           <div className="flex flex-col ml-[3rem]">
             <p>Pickup Name: </p>
-            <input className = "w-[300px]"></input>
+            <input className="w-[300px]"></input>
           </div>
 
           <div className="flex flex-col ml-[3rem]">
             <p>Email Address: </p>
-            <input className = "w-[300px]" ></input>
+            <input className="w-[300px]"></input>
           </div>
 
           <div className="flex flex-col ml-[3rem]">
             <p>Phone Number: </p>
-            <input className = "w-[300px]"></input>
+            <input className="w-[300px]"></input>
           </div>
 
+          <div className="flex justify-end w-full pr-[3rem]">
+            <button className="bg-blue-500 text-white px-4 py-2 rounded">Submit</button>
+          </div>
         </container>
 
-        
-        <img className = "h-[300px] w-auto"src="/venmo-QR.jpg"></img>
-         
-
+        <img className="h-[300px] w-auto" src="/venmo-QR.jpg"></img>
       </div>
     </>
   );
