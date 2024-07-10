b34be06fbf091c3a97988bc2c37bb004132994c4
juiceman1014 <justintran1014@gmail.com>
2024-07-09 23:48:26 -0700

    added section for other menu items

--- client/src/app/menu/page.jsx
+++ client/src/app/menu/page.jsx
@@ -34,6 +34,28 @@ const menu = () => {
             <button className = "pr-[20px] ml-auto">add to cart</button>
           </mooncake>
         </mooncakecontainer>
+
+        <h1 className = "mt-[20px] text-2xl text-center">Cheesecake</h1>
+        <cheesecakecontainer className = "flex flex-col">
+          <cheesecake className = "flex flex-row"> 
+            <img className = "h-[200px] w-auto" src="https://cdn.theatlantic.com/media/mt/science/cat_caviar.jpg"></img>
+            <div className = "flex flex-col justify-between">
+              <p>Name</p>
+              <p>Price</p>
+              <p>Description</p>
+            </div>
+            <button className = "pr-[20px] ml-auto">add to cart</button>
+          </cheesecake>
+          <cheesecake className = "flex flex-row"> 
+            <img className = "h-[200px] w-auto" src="https://cdn.theatlantic.com/media/mt/science/cat_caviar.jpg"></img>
+            <div className = "flex flex-col justify-between">
+              <p>Name</p>
+              <p>Price</p>
+              <p>Description</p>
+            </div>
+            <button className = "pr-[20px] ml-auto">add to cart</button>
+          </cheesecake>
+        </cheesecakecontainer>
       </container>
     </>
   );
