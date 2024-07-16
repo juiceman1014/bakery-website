1e79ec2042898e9568e8e4db02c8c6ed9fd99659
juiceman1014 <justintran1014@gmail.com>
2024-07-15 20:27:32 -0700

    adjusted styling on sign in button

--- client/src/app/login/page.jsx
+++ client/src/app/login/page.jsx
@@ -71,7 +71,7 @@ const Login = () => {
               </div>
               <button
                 type="submit"
-                class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                class="w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 border border-black"
               >
                 Sign in
               </button>
