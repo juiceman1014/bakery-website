c7fcfeff25e0e8f009a451a59b784fe8d3a2c675
juiceman1014 <justintran1014@gmail.com>
2024-07-16 21:39:55 -0700

    alert is shown for successful or unsuccessful regisration

--- client/src/app/register/page.jsx
+++ client/src/app/register/page.jsx
@@ -1,15 +1,16 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
-import { useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [message, setMessage] = useState("Hi");
+  const [message, setMessage] = useState("");
+  const [messageType, setMessageType] = useState("");
 
-  const handleRegister = async () => {
+  const handleRegister = async (e) => {
+    e.preventDefault(); // Prevent default form submission
     try {
       const response = await axios.post("http://localhost:8000/register", {
         email,
@@ -17,24 +18,39 @@ const Register = () => {
       });
       console.log(response.data);
       setMessage(response.data.message);
+      setMessageType(response.data.status);
     } catch (error) {
       console.error("There was an error registering!", error);
+      setMessage("There was an error registering!");
+      setMessageType("error"); // Set message type to error
     }
   };
 
   return (
     <section className="bg-white">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 ">
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
               Register
             </h1>
-            <form className="space-y-4 md:space-y-6" action="#">
+            {message && (
+              <div
+                className={`p-4 mb-4 text-sm ${
+                  messageType === "success"
+                    ? "text-green-700 bg-green-100"
+                    : "text-red-700 bg-red-100"
+                } rounded-lg`}
+                role="alert"
+              >
+                {message}
+              </div>
+            )}
+            <form className="space-y-4 md:space-y-6" onSubmit={handleRegister}>
               <div>
                 <label
-                  for="email"
-                  className="block mb-2 text-sm font-medium text-gray-900 "
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900"
                 >
                   Your email
                 </label>
@@ -43,16 +59,16 @@ const Register = () => {
                   name="email"
                   id="email"
                   value={email}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5   "
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                   placeholder="name@email.com"
                   onChange={(e) => setEmail(e.target.value)}
-                  required=""
+                  required
                 />
               </div>
               <div>
                 <label
-                  for="password"
-                  className="block mb-2 text-sm font-medium text-gray-900 "
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900"
                 >
                   Password
                 </label>
@@ -63,8 +79,8 @@ const Register = () => {
                   placeholder="••••••••"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5   "
-                  required=""
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  required
                 />
               </div>
               <div className="flex items-center justify-between">
@@ -74,13 +90,12 @@ const Register = () => {
                       id="remember"
                       aria-describedby="remember"
                       type="checkbox"
-                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300  dark:border-gray-600 dark:focus:ring-primary-600 "
-                      required=""
+                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:border-gray-600 dark:focus:ring-primary-600"
                     />
                   </div>
                   <div className="ml-3 text-sm">
                     <label
-                      for="remember"
+                      htmlFor="remember"
                       className="text-gray-500 dark:text-gray-300"
                     >
                       Remember me
@@ -97,17 +112,13 @@ const Register = () => {
               <button
                 type="submit"
                 className="w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 border border-black"
-                onClick={handleRegister}
               >
                 Register
               </button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Already have an account yet?{" "}
                 <Link href="/login">
-                  <button
-                    href="#"
-                    className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                  >
+                  <button className="font-medium text-primary-600 hover:underline dark:text-primary-500">
                     Sign in
                   </button>
                 </Link>
