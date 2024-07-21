183ffabf84062b4d465a5048652d25b66df8e5fc
juiceman1014 <justintran1014@gmail.com>
2024-07-20 17:56:24 -0700

    added login function

--- client/src/app/login/page.jsx
+++ client/src/app/login/page.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Link from "next/link";
+import axios from "axios";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [messageType, setMessageType] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault(); // Prevent default form submission
+    try {
+      const response = await axios.post("http://localhost:8000/login", {
+        email,
+        password,
+      });
+      console.log(response.data);
+      setMessage(response.data.message);
+      setMessageType(response.data.status);
+    } catch (error) {
+      console.error("There was an error signing in!", error);
+      setMessage("There was an error signing in!");
+      setMessageType("error"); // Set message type to error
+    }
+  };
+
   return (
     <section class="bg-white">
       <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -10,7 +34,19 @@ const Login = () => {
             <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
               Sign in to your account
             </h1>
-            <form class="space-y-4 md:space-y-6" action="#">
+            {message && (
+              <div
+                className={`p-4 mb-4 text-sm ${
+                  messageType === "success"
+                    ? "text-green-700 bg-green-100"
+                    : "text-red-700 bg-red-100"
+                } rounded-lg`}
+                role="alert"
+              >
+                {message}
+              </div>
+            )}
+            <form class="space-y-4 md:space-y-6" onSubmit = {handleLogin}>
               <div>
                 <label
                   for="email"
@@ -24,7 +60,9 @@ const Login = () => {
                   id="email"
                   class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5   "
                   placeholder="name@company.com"
-                  required=""
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
                 />
               </div>
               <div>
@@ -40,7 +78,9 @@ const Login = () => {
                   id="password"
                   placeholder="••••••••"
                   class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5   "
-                  required=""
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
                 />
               </div>
               <div class="flex items-center justify-between">
@@ -70,23 +110,23 @@ const Login = () => {
                   Forgot password?
                 </a>
               </div>
-              
-                <button
-                  type="submit"
-                  class="w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 border border-black"
-                >
-                  Sign in
-                </button>
-        
+
+              <button
+                type="submit"
+                class="w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 border border-black"
+              >
+                Sign in
+              </button>
+
               <p class="text-sm font-light text-gray-500 dark:text-gray-400">
                 Don’t have an account yet?{" "}
-                <Link href = "/register">
-                <button
-                  href="#"
-                  class="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                >
-                  Sign up
-                </button>
+                <Link href="/register">
+                  <button
+                    href="#"
+                    class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  >
+                    Sign up
+                  </button>
                 </Link>
               </p>
             </form>
