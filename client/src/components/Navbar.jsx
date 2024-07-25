cadec1d471f317a36fbaffc162cd8cc7544a3c51
juiceman1014 <justintran1014@gmail.com>
2024-07-24 22:31:56 -0700

    navbar changes depending on whether or not you're logged in

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+"use client";
+import React, { useContext } from "react";
 import Link from "next/link";
+import { UserContext } from "../app/context/UserContext";
 
 const Navbar = () => {
+  const { user, setUser } = useContext(UserContext);
+  const { logout } = useContext(UserContext);
+
+  const handleLogout = async () => {
+    await logout();
+  };
+
   return (
     <div className="h-screen">
       <div className="flex flex-col justify-between h-full w-24 bg-orange-100 items-center py-4 fixed">
@@ -23,10 +32,22 @@ const Navbar = () => {
           </button>
         </div>
 
-        <div className="flex flex-col space-y-2">
-          <button className="hover:bg-orange-200 p-2 rounded-md text-sm font-medium">
-            <Link href="/login">Login</Link>
-          </button>
+        <div className="flex flex-col items-center space-y-2">
+          {user ? (
+            <>
+              <div className="rounded-md text-sm font-medium break-words w-20">{user}</div>
+              <button className="hover:bg-orange-200 p-2 rounded-md text-sm font-medium" onClick={handleLogout}>
+                Logout
+              </button>
+              <button className="hover:bg-orange-200 p-2 rounded-md text-sm font-medium">
+                <Link href="/past-orders">Past Orders</Link>
+              </button>
+            </>
+          ) : (
+            <button className="hover:bg-orange-200 p-2 rounded-md text-sm font-medium">
+              <Link href="/login">Login</Link>
+            </button>
+          )}
         </div>
 
         <div className="hover:bg-orange-200 p-2 rounded-full">
