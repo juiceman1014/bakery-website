39d41fe42aa4df77c1d26f2c7d23fb839eae0109
juiceman1014 <justintran1014@gmail.com>
2024-08-21 21:02:46 -0700

    cart now displays all items

--- client/src/app/cart/page.jsx
+++ client/src/app/cart/page.jsx
@@ -1,42 +1,55 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState, useContext} from "react";
+import axios from "axios";
 import Link from "next/link";
+import { UserContext } from "../context/UserContext.jsx";
 
 const Cart = () => {
+  const{user} = useContext(UserContext);
+  const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    const fetchCartItems = async() => {
+      if (user) {
+        try{
+          const response = await axios.get(`http://localhost:8000/cart/${user.ID}`);
+          setCartItems(response.data);
+        } catch(error){
+          console.error("There was an error fetching the cart items!", error);
+        }
+      } else{
+        const guestCart = JSON.parse(localStorage.getItem('guest_cart')) || [];
+        setCartItems(guestCart);
+      }
+    };
+    
+    fetchCartItems();
+  }, [user]);
+
+  const calculateTotal = () => {
+    return cartItems.reduce((total, item) => total + (item.price * item.quantity), 0).toFixed(2);
+  };
+
+  const total = calculateTotal();
+
   return (
     <>
       <div className="flex flex-row justify-center items-center h-screen">
         <container className="flex flex-col justify-center items-center bg-orange-100 h-[500px] w-[500px] my-auto justify-evenly">
           
           <div className="h-1/6 w-6/12 flex flex-col justify-center">
-            <div className="flex justify-between">
-              <p className="break-all">
-                Mooncake Mooncake Mooncake Mooncake Mooncake Mooncake Mooncake
-              </p>
-              <p>Price</p>
+          {cartItems.map((item) => (
+            <div key = {item.ID} className="flex justify-between">
+              <p>{item.item_name}</p>
+              <p>{item.price}</p>
+              <p>Quantity: {item.quantity}</p>
             </div>
-
-            <div className="flex justify-between">
-              <p>Item2</p>
-              <p>Price</p>
-            </div>
-            <div className="flex justify-between">
-              <p>Item3</p>
-              <p>Price</p>
-            </div>
-          </div>
-
-          <div className="h-1/6 w-6/12 bg-slate-100 flex flex-col justify-center">
-            <div className="flex justify-between">
-              <p>Subtotal</p>
-              <p>Price</p>
-            </div>
-            <div className="flex justify-between">
-              <p>Tax</p>
-              <p>Price</p>
-            </div>
+          ))}
+        
             <div className="flex justify-between">
               <p>Total</p>
-              <p>Price</p>
+              <p>${total}</p>
             </div>
           </div>
 
