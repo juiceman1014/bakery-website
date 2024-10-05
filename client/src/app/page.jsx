66aebe439f64ce14d3f726f2870bb7d986cf4855
juiceman1014 <justintran1014@gmail.com>
2024-10-05 15:52:07 -0700

    EC2 Testing

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -25,30 +25,7 @@ export default function Home() {
           <div className="flex flex-col justify-center w-full">
             <h1 className="font-bold text-center">About</h1>
             <p className="mt-10 ml-5">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-              mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-              voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-              fugiat iusto fuga praesentium optio, eaque rerum! Provident
-              similique accusantium nemo autem. Veritatis obcaecati tenetur iure
-              eius earum ut molestias architecto voluptate aliquam nihil,
-              eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-              tenetur error, harum nesciunt ipsum debitis quas aliquid.
-              Reprehenderit, quia. Quo neque error repudiandae fuga? Ipsa
-              laudantium molestias eos sapiente officiis modi at sunt excepturi
-              expedita sint? Sed quibusdam recusandae alias error harum maxime
-              adipisci amet laborum. Perspiciatis minima nesciunt dolorem!
-              Officiis iure rerum voluptates a cumque velit quibusdam sed amet
-              tempora. Sit laborum ab, eius fugit doloribus tenetur fugiat,
-              temporibus enim commodi iusto libero magni deleniti quod quam
-              consequuntur! Commodi minima excepturi repudiandae velit hic
-              maxime doloremque. Quaerat provident commodi consectetur veniam
-              similique ad earum omnis ipsum saepe, voluptas, hic voluptates
-              pariatur est explicabo fugiat, dolorum eligendi quam cupiditate
-              excepturi mollitia maiores labore suscipit quas? Nulla, placeat.
-              Voluptatem quaerat non architecto ab laudantium modi minima sunt
-              esse temporibus sint culpa, recusandae aliquam numquam totam
-              ratione voluptas quod exercitationem fuga. Possimus quis earum
-              veniam quasi aliquam eligendi, placeat qui corporis!
+              Hello Im Bin
             </p>
           </div>
         </about>
