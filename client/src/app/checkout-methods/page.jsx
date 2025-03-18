f6e25d281c3b7dbb41201ee2ffd0e729c442e962
Danh Pham <danhpham612@gmail.com>
2025-03-18 13:41:35 -0700

    edited checkout methods UI

--- client/src/app/checkout-methods/page.jsx
+++ client/src/app/checkout-methods/page.jsx
@@ -3,23 +3,24 @@ import Link from "next/link";
 
 const Checkout = () => {
   return (
-    <div className="flex flex-row justify-center items-center h-screen">
-      <container className="flex flex-col justify-center items-center bg-orange-100 h-3/6 w-6/12 justify-evenly">
-        <Link
-          href="/checkout-methods/pickup"
-          className="h-1/6 w-6/12 bg-slate-100 flex justify-center"
-        >
-          <button>Pick up</button>
-        </Link>
-
-        <Link
-          href="/checkout-methods/delivery"
-          className="h-1/6 w-6/12 bg-slate-100 flex justify-center"
-        >
-          <button>Delivery</button>
-        </Link>
-      </container>
-    </div>
+    <>
+      <div className="flex flex-row justify-center items-center h-screen bg-gray-100">
+        <div className="flex flex-col justify-center items-center bg-orange-100 h-[500px] w-[500px] rounded-lg shadow-lg p-6">
+          <div className="flex flex-col w-10/12 mt-4 space-y-4">
+            <Link href="/checkout-methods/delivery" className="w-full">
+              <button className="w-full bg-white py-2 px-4 rounded-lg shadow-md hover:bg-gray-200 transition duration-200">
+                Delivery
+              </button>
+            </Link>
+            <Link href="/checkout-methods/pickup" className="w-full">
+              <button className="w-full bg-orange-500 text-white py-2 px-4 rounded-lg shadow-md hover:bg-orange-600 transition duration-200">
+                Pick Up
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
